b83bbcfeb227ebd67937bda75fa7861de3cb43c8
Ben Klein <99753992+043a7e@users.noreply.github.com>
2022-08-26 11:58:12 -0400

    Update massdecompressor.py

--- massdecompressor.py
+++ massdecompressor.py
@@ -4,8 +4,8 @@ import shutil
 
 ctnumber = input('What is your CT or case number? ')
 cdwcache = input('Drag your folder here: ')
-ctloc, trash = os.path.split(cdwcache)
 cdw = cdwcache.strip('\"' "\'")
+ctloc, trash = os.path.split(cdw)
 ctfoldercache = str(ctloc) + '/' + ctnumber
 ctfolder = ctfoldercache.strip('\"' "\'")
 ctoriginals = ctfolder + '/_originals/'
@@ -14,28 +14,32 @@ zipsfolder = cdw + '/zips/'
 os.mkdir(ctfolder)
 os.mkdir(ctoriginals)
 os.mkdir(ctdata)
-os.mkdir(zipsfolder)
 
 def decompressor():
-    if file.endswith('.zip'):
-        with zipfile.ZipFile(file, 'r') as topzip:
-            topzip.extractall()
-            zipend = zipsfolder + '/' + file
-            os.rename(file , zipend)
+    os.mkdir(zipsfolder)
+    for file in os.listdir(os.getcwd()):
+        print('Decompressing ' + file)
+        if file.endswith('.zip'):
+            with zipfile.ZipFile(file, 'r') as topzip:
+                topzip.extractall()
+                topzip.close()
+                zipend = zipsfolder + '/' + file
+                os.rename(file , zipend)
+        else:
+            continue
 
 def copyoriginals():
-    for file in os.listdir(os.getcwd()):
-        if file.endswith('.zip'):
-            originalend = ctoriginals + '/' + file
-            shutil.copy2(file , originalend)
+    print('Moving ' + file)
+    originalend = ctoriginals + '/' + file
+    shutil.copy2(file , originalend)
 
+        
 os.chdir(cdw)
 
-for file in os.listdir(os.getcwd()):
+for file in os.listdir('.'):
     copyoriginals()
-while any(file.endswith('.zip') for file in os.listdir(os.getcwd())):
-    for file in os.listdir(os.getcwd()):
-        decompressor()
+while any(file.endswith('.zip') for file in os.listdir('.')):
+    decompressor()
 
 shutil.rmtree(zipsfolder)
 shutil.move(os.getcwd() , ctdata)
